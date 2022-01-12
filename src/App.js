c96ffaa6378c0d4007b28d8b884849ad78bd5dbf
maddyonline <madhavjha@gmail.com>
2022-01-12 15:58:41 -0700

    attempt 1

--- src/App.js
+++ src/App.js
@@ -12,9 +12,8 @@ import {
   useRecoilValue,
 } from 'recoil';
 
-var rafId;
-
 const isVideoPlaying = video => !!(video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2);
+
 
 const posesState = atom({
   key: 'posesState', // unique ID (with respect to other atoms/selectors)
@@ -22,68 +21,74 @@ const posesState = atom({
 });
 
 
-function MyApp() {
-  const mediapipePoseDetector = React.useRef(null);
-  const [poses, setPoses] = useRecoilState(posesState);
-  const videoRef = React.useRef(null);
-  const canvasRef = React.useRef(null);
-  const ctxRef = React.useRef(null);
-  React.useEffect(() => {
-    if (canvasRef.current) {
-      ctxRef.current = canvasRef.current.getContext('2d');
-    }
 
-  }, [canvasRef.current])
+function usePoseTracker({ videoRef, posesState }) {
+  const poseDetector = React.useRef(null);
+  const [_, setPoses] = useRecoilState(posesState);
+
   React.useEffect(() => {
-    const init = async () => {
-      console.log(mpPose.VERSION)
-      mediapipePoseDetector.current = await posedetection.createDetector(posedetection.SupportedModels.BlazePose, {
+    var rafId;
+    const runFrame = async () => {
+      if (videoRef.current && poseDetector.current) {
+        const poses = await poseDetector.current.estimatePoses(
+          videoRef.current,
+          { maxPoses: 1, flipHorizontal: false });
+        setPoses(poses);
+      }
+      rafId = requestAnimationFrame(runFrame);
+    }
+    const start = async () => {
+      console.log("initializing mediapose", mpPose.VERSION)
+      poseDetector.current = await posedetection.createDetector(posedetection.SupportedModels.BlazePose, {
         runtime: 'mediapipe',
         modelType: 'heavy',
         solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/pose@${mpPose.VERSION}`
       });
-      console.log({ current: mediapipePoseDetector.current })
+      await runFrame();
+
     }
-    init()
-    return () => mediapipePoseDetector.current ?
-      mediapipePoseDetector.current.dispose() : null
+    start();
+
+    return () => {
+      // cleanup
+      if (rafId) {
+        cancelAnimationFrame(rafId);
+      }
+      if (poseDetector.current) {
+        poseDetector.current.dispose();
+      }
+    }
   }, [])
-  return (
-    <div className="App">
 
-      <div style={{ display: "flex", border: "1px solid red" }}>
-        <video style={{ width: '400px', height: '225px' }} ref={videoRef} autoPlay>
-          <source src="/home-workout.mp4" type="video/mp4" />
-        </video>
-        <canvas style={{ width: '400px', height: '225px' }} ref={canvasRef} />
-      </div>
 
-      <button onClick={() => {
-        if (isVideoPlaying(videoRef.current)) {
-          videoRef.current.pause();
-        } else {
-          videoRef.current.play()
-        }
-      }}>Play/Pause</button>
+}
 
-      <button onClick={async () => {
-        async function runFrame() {
-          const poses = await mediapipePoseDetector.current.estimatePoses(
-            videoRef.current,
-            { maxPoses: 1, flipHorizontal: false });
-          rafId = requestAnimationFrame(runFrame);
-          setPoses(poses);
-        }
-        if (rafId) {
-          cancelAnimationFrame(rafId);
-        }
-        runFrame();
-      }}>Start</button>
 
-      <div>{poses?.length > 0 ? JSON.stringify(poses[0]["keypoints"][0].x) : "null"}</div>
+function RenderPosesSimple() {
+  const poses = useRecoilValue(posesState);
+  return <>
+    {poses && poses.length > 0 ? `${poses[0].keypoints[0].x}` : "nothing to show"}
+  </>
+}
 
-    </div>
-  );
+function MyApp() {
+  const videoRef = React.useRef(null);
+
+  usePoseTracker({ videoRef, posesState })
+
+  return <>
+    <video style={{ width: '400px', height: '225px' }} ref={videoRef} autoPlay>
+      <source src="/home-workout.mp4" type="video/mp4" />
+    </video>
+    <button onClick={() => {
+      if (isVideoPlaying(videoRef.current)) {
+        videoRef.current.pause();
+      } else {
+        videoRef.current.play()
+      }
+    }}>Play/Pause</button>
+    <RenderPosesSimple />
+  </>
 }
 
 export default () => {
