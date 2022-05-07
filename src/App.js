6b97bcf30a0ebc391ff2772591b976a627018208
Madhav Jha <madhavjha@gmail.com>
2022-05-07 09:32:38 -0700

    emitter/listener based approach

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import "./App.css";
 
-import * as poseDetection from "@tensorflow-models/pose-detection";
-import * as tf from "@tensorflow/tfjs-core";
-// Register WebGL backend.
-import "@tensorflow/tfjs-backend-webgl";
-
 import { PoseArtist } from "./draw_utils";
+import makePoseDetector from "./PoseDetector";
 
 import React from "react";
-import { RecoilRoot, atom, useRecoilState, useRecoilValue } from "recoil";
 
 var POSE_DETECTOR_MODEL = "movenet"; // "blazepose"
-
-function getModelDetails(poseDetectorModel) {
-  const model =
-    poseDetectorModel === "movenet"
-      ? poseDetection.SupportedModels.MoveNet
-      : poseDetection.SupportedModels.BlazePose;
-  const modelConfig =
-    poseDetectorModel === "movenet"
-      ? {
-          modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING,
-        }
-      : {
-          runtime: "tfjs",
-          enableSmoothing: true,
-          modelType: "full",
-        };
-  return [model, modelConfig];
-}
 
 const isVideoPlaying = (video) =>
   !!(
@@ -38,115 +15,92 @@ const isVideoPlaying = (video) =>
     video.readyState > 2
   );
 
-const posesState = atom({
-  key: "posesState", // unique ID (with respect to other atoms/selectors)
-  default: [], // default value (aka initial value)
-});
-
-function usePoseTracker({ videoRef, posesState }) {
-  const poseDetector = React.useRef(null);
-  const [_, setPoses] = useRecoilState(posesState);
-
+function RenderPosesSimple({ poseDetectorRef }) {
+  const listener = React.useCallback((poses) => console.log(poses), []);
   React.useEffect(() => {
-    var rafId;
-    const runFrame = async () => {
-      if (videoRef.current && poseDetector.current) {
-        const poses = await poseDetector.current.estimatePoses(
-          videoRef.current,
-          { maxPoses: 1, flipHorizontal: false }
-        );
-        setPoses(poses);
-      }
-      rafId = requestAnimationFrame(runFrame);
-    };
-    const start = async () => {
-      const [model, modelConfig] = getModelDetails(POSE_DETECTOR_MODEL);
-      console.log(
-        `creating detector with model ${model} and config ${JSON.stringify(
-          modelConfig
-        )}`
-      );
-      const detector = await poseDetection.createDetector(model, modelConfig);
-      poseDetector.current = detector;
-      await runFrame();
-    };
-    start();
-
-    return () => {
-      // cleanup
-      if (rafId) {
-        cancelAnimationFrame(rafId);
-      }
-      if (poseDetector.current) {
-        poseDetector.current.dispose();
-      }
-    };
-  }, [setPoses, videoRef]);
-}
-
-function RenderPosesSimple() {
-  const poses = useRecoilValue(posesState);
+    poseDetectorRef.addListener("poses", listener);
+    return () => poseDetectorRef.removeListener("poses", listener);
+  }, []);
   return (
     <>
-      {poses && poses.length > 0
-        ? `${poses[0].keypoints[0].x}`
-        : "nothing to show"}
+      <div>Poses Simple</div>
     </>
   );
 }
 
-function RenderPose({ model, modelConfig }) {
-  const canvasRef = React.useRef(null);
-  const poses = useRecoilValue(posesState);
-  const poseArtist = React.useRef(null);
+// function RenderPose({ model, modelConfig }) {
+//   const canvasRef = React.useRef(null);
+//   const poses = useRecoilValue(posesState);
+//   const poseArtist = React.useRef(null);
 
-  React.useEffect(() => {
-    if (!poseArtist.current && canvasRef.current) {
-      const ctx = canvasRef.current.getContext("2d");
-      const [model, modelConfig] = getModelDetails(POSE_DETECTOR_MODEL);
-      poseArtist.current = new PoseArtist(ctx, model, modelConfig);
-    }
-  }, []);
+//   React.useEffect(() => {
+//     if (!poseArtist.current && canvasRef.current) {
+//       const ctx = canvasRef.current.getContext("2d");
+//       const [model, modelConfig] = getModelDetails(POSE_DETECTOR_MODEL);
+//       poseArtist.current = new PoseArtist(ctx, model, modelConfig);
+//     }
+//   }, []);
 
-  React.useEffect(() => {
-    // var rafId;
-    const draw = async () => {
-      if (canvasRef.current && poses && poses.length > 0) {
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext("2d");
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        if (poseArtist.current) {
-          // console.log("drawing..", canvas.width, canvas.height);
-          poseArtist.current.drawResults(poses);
-        }
-        // drawPose(
-        //   poses[0],
-        //   canvasRef.current.getContext("2d"),
-        //   poseDetection.SupportedModels.BlazePose,
-        //   0
-        // );
-      }
-    };
-    draw();
-    // return () => {
-    //   if (rafId) {
-    //     cancelAnimationFrame(rafId);
-    //   }
-    // }
-  }, [poses]);
+//   React.useEffect(() => {
+//     // var rafId;
+//     const draw = async () => {
+//       if (canvasRef.current && poses && poses.length > 0) {
+//         const canvas = canvasRef.current;
+//         const ctx = canvas.getContext("2d");
+//         ctx.clearRect(0, 0, canvas.width, canvas.height);
+//         if (poseArtist.current) {
+//           // console.log("drawing..", canvas.width, canvas.height);
+//           poseArtist.current.drawResults(poses);
+//         }
+//         // drawPose(
+//         //   poses[0],
+//         //   canvasRef.current.getContext("2d"),
+//         //   poseDetection.SupportedModels.BlazePose,
+//         //   0
+//         // );
+//       }
+//     };
+//     draw();
+//     // return () => {
+//     //   if (rafId) {
+//     //     cancelAnimationFrame(rafId);
+//     //   }
+//     // }
+//   }, [poses]);
 
-  return (
-    <div style={{ border: "1px solid red" }}>
-      <div>Hello</div>
-      <canvas width={600} height={400} ref={canvasRef}></canvas>
-    </div>
-  );
-}
+//   return (
+//     <div style={{ border: "1px solid red" }}>
+//       <div>Hello</div>
+//       <canvas width={600} height={400} ref={canvasRef}></canvas>
+//     </div>
+//   );
+// }
 
 function MyApp() {
   const videoRef = React.useRef(null);
+  const poseDetectorRef = React.useRef(null);
 
-  usePoseTracker({ videoRef, posesState });
+  const modelReady = React.useCallback(() => console.log("Model Ready"));
+  const onPoses = React.useCallback((poses) =>
+    console.log("poses", poses && poses.length)
+  );
+  const onPoses2 = React.useCallback((poses) =>
+    console.log("poses", poses && poses[0].keypoints)
+  );
+
+  React.useEffect(() => {
+    console.log("here");
+    if (videoRef.current && !poseDetectorRef.current) {
+      console.log("here2");
+      const video = videoRef.current;
+      const poseDetector = makePoseDetector(video, modelReady);
+
+      poseDetectorRef.current = poseDetector;
+      window.poseDetectorRef = poseDetectorRef;
+    }
+    return () =>
+      poseDetectorRef.current && poseDetectorRef.current.removeAllListeners();
+  }, [videoRef.current]);
 
   return (
     <>
@@ -157,25 +111,24 @@ function MyApp() {
         onClick={() => {
           if (isVideoPlaying(videoRef.current)) {
             videoRef.current.pause();
+            poseDetectorRef.current.removeAllListeners();
           } else {
+            if (poseDetectorRef.current) {
+              poseDetectorRef.current.removeAllListeners();
+              poseDetectorRef.current.on("pose", onPoses);
+            }
             videoRef.current.play();
           }
         }}
       >
         Play/Pause
       </button>
-      <RenderPosesSimple />
-      <RenderPose videoRef={videoRef} />
     </>
   );
 }
 
 const FullApp = () => {
-  return (
-    <RecoilRoot>
-      <MyApp />
-    </RecoilRoot>
-  );
+  return <MyApp />;
 };
 
 export default FullApp;
---
+++ src/PoseDetector.js
@@ -0,0 +1,168 @@
+// Copyright (c) 2018 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/*
+PoseNet
+The original PoseNet model was ported to TensorFlow.js by Dan Oved.
+*/
+
+import EventEmitter from "events";
+import * as posedetection from "@tensorflow-models/pose-detection";
+import * as tf from "@tensorflow/tfjs-core";
+// Register WebGL backend.
+import "@tensorflow/tfjs-backend-webgl";
+
+function callCallback(promise, callback) {
+  if (!callback) return promise;
+  return new Promise((resolve, reject) => {
+    promise
+      .then((result) => {
+        callback(undefined, result);
+        resolve(result);
+      })
+      .catch((error) => {
+        callback(error);
+        reject(error);
+      });
+  });
+}
+
+
+
+function getModelDetails(poseDetectorModel) {
+  const model =
+    poseDetectorModel === "movenet"
+      ? posedetection.SupportedModels.MoveNet
+      : posedetection.SupportedModels.BlazePose;
+  const modelConfig =
+    poseDetectorModel === "movenet"
+      ? {
+          modelType: posedetection.movenet.modelType.SINGLEPOSE_LIGHTNING,
+        }
+      : {
+          runtime: "tfjs",
+          enableSmoothing: true,
+          modelType: "full",
+        };
+  return [model, modelConfig];
+}
+
+class PoseDetector extends EventEmitter {
+  constructor(video, options, callback) {
+    super();
+    this.video = video;
+    this.poseDetectorModel = options.poseDetectorModel;
+    this.ready = callCallback(this.load(), callback);
+    this.then = this.ready.then;
+  }
+
+  async load() {
+    const [model, modelConfig] = getModelDetails(this.poseDetectorModel);
+
+    this.net = await posedetection.createDetector(model, modelConfig);
+
+    if (this.video) {
+      if (this.video.readyState === 0) {
+        await new Promise((resolve) => {
+          this.video.onloadeddata = () => resolve();
+        });
+      }
+      this.detectPose();
+    }
+    return this;
+  }
+
+  getInput(inputOr) {
+    let input;
+    if (
+      inputOr instanceof HTMLImageElement ||
+      inputOr instanceof HTMLVideoElement ||
+      inputOr instanceof HTMLCanvasElement ||
+      inputOr instanceof ImageData
+    ) {
+      input = inputOr;
+    } else if (
+      typeof inputOr === "object" &&
+      (inputOr.elt instanceof HTMLImageElement ||
+        inputOr.elt instanceof HTMLVideoElement ||
+        inputOr.elt instanceof ImageData)
+    ) {
+      input = inputOr.elt; // Handle p5.js image and video
+    } else if (
+      typeof inputOr === "object" &&
+      inputOr.canvas instanceof HTMLCanvasElement
+    ) {
+      input = inputOr.canvas; // Handle p5.js image
+    } else {
+      input = this.video;
+    }
+
+    return input;
+  }
+
+  /**
+   * Given an image or video, returns an array of objects containing pose estimations
+   *    using single or multi-pose detection.
+   * @param {HTMLVideoElement || p5.Video || function} inputOr
+   * @param {function} cb
+   */
+  /* eslint max-len: ["error", { "code": 180 }] */
+  async detectPose(inputOr, cb) {
+    const input = this.getInput(inputOr);
+
+    const result = await this.net.estimatePoses(input, {
+      maxPoses: 1,
+      flipHorizontal: false,
+    });
+    // const poseWithParts = this.mapParts(pose);
+    // const result = [{ pose: poseWithParts, skeleton: this.skeleton(pose.keypoints) }];
+    this.emit("pose", result);
+
+    if (this.video) {
+      return tf.nextFrame().then(() => this.detectPose());
+    }
+
+    if (typeof cb === "function") {
+      cb(result);
+    }
+
+    return result;
+  }
+}
+
+const makePoseDetector = (videoOrOptionsOrCallback, optionsOrCallback, cb) => {
+  let video;
+  let options = {
+    poseDetectorModel: "movenet",
+  };
+  let callback = cb;
+
+  if (videoOrOptionsOrCallback instanceof HTMLVideoElement) {
+    video = videoOrOptionsOrCallback;
+  } else if (
+    typeof videoOrOptionsOrCallback === "object" &&
+    videoOrOptionsOrCallback.elt instanceof HTMLVideoElement
+  ) {
+    video = videoOrOptionsOrCallback.elt; // Handle a p5.js video element
+  } else if (typeof videoOrOptionsOrCallback === "object") {
+    options = videoOrOptionsOrCallback;
+  } else if (typeof videoOrOptionsOrCallback === "function") {
+    callback = videoOrOptionsOrCallback;
+  }
+
+  if (typeof optionsOrCallback === "object") {
+    options = optionsOrCallback;
+  } else if (typeof optionsOrCallback === "string") {
+    options.poseDetectorModel = optionsOrCallback;
+  }
+
+  if (typeof optionsOrCallback === "function") {
+    callback = optionsOrCallback;
+  }
+
+  return new PoseDetector(video, options, callback);
+};
+
+export default makePoseDetector;
